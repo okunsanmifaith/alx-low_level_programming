6c33894c3f989ef1e8ed7e355525191019214196
Okunsanmi Faith Abike <Okunsanmi Faith Abike>
2022-07-03 15:17:11 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,24 +1,23 @@
-#include <stdlio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
- */
+ * Return: 0(succes)
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	
+        if(n == 0)
+                printf("%i is zer\n", n);
+        else if (n < 0)
+                printf("%i is negative\n", n);
+        else
+                printf("%i is positive\n", n);
 
-	if(n == 0)
-		printf("%i is zer\n", n);
-	else if (n < 0)
-		printf("%i is negative\n", n);
-	else
-		printf("%i is positive\n", n);
-
-	return (0);
+        return (0);
 }
