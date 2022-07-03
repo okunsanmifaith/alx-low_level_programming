559434f6bd950055b0eb7b809fc4c4f5be850602
Okunsanmi Faith Abike <Okunsanmi Faith Abike>
2022-07-03 06:39:40 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,15 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("single digit number are: %d\n, num);
+	return (0);
+}
