3f663b86017fff50e8817d0e15913a8a3c03c9f1
Okunsanmi Faith Abike <Okunsanmi Faith Abike>
2022-07-09 13:28:07 -0700

    lowercase 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - is a function that prints 10 times the alphabet in lowercase
+ *
+ * Return: Always 0(success)
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; printf("abcdefghijklmnopqrstuvwxyz\n", i++));
+	_putchar(i);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 A function that print lowercase alphabet in c programming, is amazing.
+a function that prints 10 times the alphabet in lowercase, followed by a new line.
