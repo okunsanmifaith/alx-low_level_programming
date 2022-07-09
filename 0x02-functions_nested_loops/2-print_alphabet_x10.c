94120d42d3e5550138f1dd8fab847b564e3ab8cb
Okunsanmi Faith Abike <Okunsanmi Faith Abike>
2022-07-09 14:01:50 -0700

    x10 lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,8 +9,9 @@ void print_alphabet_x10(void)
 {
 	int i, j;
 
-	for (i = 0; i < 10; i++)
+	while (i < 10)
 		for (j = 'a'; j <= 'z'; j++)
-			_putchar(j)
-				_putchar('\n');
+			_putchar(j);
+	i++;
+	_putchar('\n');
 }
