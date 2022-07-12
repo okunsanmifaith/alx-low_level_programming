90cb8a00a870f94dbec54f5ae9c3920e313a35ba
Okunsanmi Faith Abike <Okunsanmi Faith Abike>
2022-07-11 19:28:56 -0700

    print some numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#includee "main.h"
+/**
+ * print_to_98 - to print numbers
+ * @n: a variable name
+ * Return: n to 98
+ */
+void print_to_98(int n)
+{
+	for (n <= 0; n < 98; n++)
+	{
+		printf("%d, ", n);
+	}
+	return (0);
+}
