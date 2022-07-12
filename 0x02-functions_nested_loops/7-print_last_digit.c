dcd29fe9098021c6c9b21e6163db57ae496cbe3b
Okunsanmi Faith Abike <Okunsanmi Faith Abike>
2022-07-11 18:13:47 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit
+ * @i: a variable name
+ * Return: Always 0(success)
+ */
+int print_last_digit(int i)
+{
+	int x;
+
+	if (n < 0)
+		n = -n;
+	x = n % 10;
+	_putchar(x + '0');
+	return (m % 10);
+}
+
