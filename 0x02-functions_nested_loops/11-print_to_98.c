a849897666d5b35e9aab568b8e992d7a89c6e269
Okunsanmi Faith Abike <Okunsanmi Faith Abike>
2022-07-11 19:42:33 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_to_98 - to print numbers
+ * print_to_98 - print number with cmma and whitespace
  * @n: a variable name
  * Return: n to 98
  */
@@ -8,15 +8,11 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (n = n; n > 98; n--)
-		{
+		for (n = n; n <= 97; n++)
 			printf("%d, ", n);
-		}
 	}
 	else
 		for (n = n; n > 98; n--)
-		{
 			printf("%d, ", n);
-		}
 	printf("98\n");
 }
