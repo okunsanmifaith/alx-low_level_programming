69164587a10b0c33226773a34fde2b4377f40b01
Okunsanmi Faith Abike <Okunsanmi Faith Abike>
2022-07-15 04:29:01 -0700

    multiples of 5 and 3

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 0(success)
+ */
+void natural.c(int)
+{
+	int n,i;
+
+	for (n = 0; n < 1024; n++)
+	{
+		for (n % 3 == 0; || n % 5 == 0)
+		{
+			i += n;
+		}
+	}
+	_putchar('\n');
+}
