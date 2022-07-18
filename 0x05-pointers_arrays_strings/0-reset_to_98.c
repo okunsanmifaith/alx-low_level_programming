044b20fd4a13f9b712f8e164e4ef5dea16b29907
Okunsanmi Faith Abike <Okunsanmi Faith Abike>
2022-07-17 19:02:11 -0700

    task o

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - a function to update to 98
+ * @n: variable name
+ * Return: 98
+ */
+void reset_to_98(int *n)
+{
+	int n;
+	int *n = &n;
+	int n = 98;
+
+	printf("%d\n", &n);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+How to use pointers, array and string in c programming.
