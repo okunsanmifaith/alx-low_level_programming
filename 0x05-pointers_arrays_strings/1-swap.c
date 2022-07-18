bb3dfbb0fbb05710c214cd76a10c6b5dac44a8e3
Okunsanmi Faith Abike <Okunsanmi Faith Abike>
2022-07-17 19:23:26 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - function to swap number
+ * @a: the first variable name
+ * @b: the second variable name
+ * Return: success
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 98;
+	*b = 42;
+
+	printf("*a = %d, *b = %d\n", *a, *b);
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+	printf("\n*a = %d, *b = %d", *a, *b);
+
+	return (0);
+}
