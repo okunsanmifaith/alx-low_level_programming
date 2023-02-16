77d0209b0f6b2706e99dde85ce48a5e51a1c6bf5
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-02-16 14:12:12 -0800

    Print function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,14 +6,14 @@
 
 /**
  *main - Print "with proper grammar,
- *       but the outcome is a piece 
+ *       but the outcome is a piece
  *       of art,, followed by a new line.
  *
  *return: Always 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art");
+printf("with proper grammar, but the outcome is a piece of art,");
 
 return (0);
 }
