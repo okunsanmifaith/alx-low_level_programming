b261e65eb7aefe516938a0edea0a5285149e5e30
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-02-16 14:17:02 -0800

    Print function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 
 /**
- *main- Prints "with proper grammar,
+ *main - Prints with proper grammar,
  *       but the outcome is a piece
  *       of art,, followed by a new line.
  *
