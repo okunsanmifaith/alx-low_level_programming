b1c872aa270594944c5f04f36f0d23b8f53f8414
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-02-16 13:58:35 -0800

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,14 +7,14 @@
 
 
 /**
-*main - "Programming is like building a multilingual
+*main - Prints "Programming is like building a multilingual
 *        puzzle, followed by a new line.
 *
 *Return: Always 0
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle")
+  puts("\"Programming is like building a multilingual puzzle");
 
-return (0)
+  return (0);
 }
