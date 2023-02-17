7c1f73e85e259476dd38c350c29a64d80f3b55cd
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-02-17 13:59:37 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio>
+/**
+ *main - Entry ppoint
+ *
+ *Description: print lowercase alphabet except q and e
+ *Return: Always 0
+ */
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+if (c == 'e' || c == 'e')
+continue;
+putchar(c);
+putchar('\n');
+return (0);
+}
