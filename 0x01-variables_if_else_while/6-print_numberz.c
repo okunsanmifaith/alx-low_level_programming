c9c4bdd3a405a4ccedfee7a2e1ed820b1e120c07
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-02-17 14:26:03 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: Prints all single digit number of base 10
+ *Return: Always 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
