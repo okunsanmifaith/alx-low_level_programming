9536db3022c83bb2d129320891eb7cc3583f4a7d
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-02-17 14:36:31 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: Print alphabet in reverse
+ * Return: Zero value
+ */
+
+int main(void)
+{
+char n, a;
+for (a = 0; a <= 0; a++)
+putchar(a);
+for (n = 'a'; n >= 'a'; n++)
+putchar(n);
+putchar('\n');
+return (0);
+}
