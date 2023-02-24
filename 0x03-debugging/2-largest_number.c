2e458b2c181df0afbc1a73ee8f9a5d23c7086dcd
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-02-23 22:14:27 -0800

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,26 +1,5 @@
-#include <stdio.h>
 #include "main.h"
 
-/**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
-
-int main(void)
-{
-int a, b, c;
-int largest;
-
-a = 972;
-b = -98;
-c = 0;
-
-largest = largest_number(a, b, c);
-
-printf("%d is the largest number\n", largest);
-
-return (0);
-}
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
@@ -42,10 +21,6 @@ else if (b > a && a > c)
 largest = b;
 }
 else if (c > a && a > b)
-{
-largest = c;
-}
-else if (c > b && b > a)
 {
 largest = c;
 }
