be305a724a62cd7efc6aec7658db31ef21d26075
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-02-24 22:29:46 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
  */
 void print_alphabet(void)
 {
-  int i;
+int i;
 
-  for (i = 'a'; i <= 'z'; i++)
-    {
-      _putchar(i);
-    }
-  _putchar('\n');
+for (i = 'a'; i <= 'z'; i++)
+}
+putchar(i);
+}
+putchar('\n');
 }
