8fbcf58ed8505c87280bcb1fc97f890aee0943e6
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-02-25 09:16:40 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *print_sign - prints the sign of numbers
+ *@n: integer
+ *Return: 1 if n is greater than zero, 0 if n is zero
+ *and -1 if n is less than zero
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+printf("+");
+return (1);
+}
+else if (n == 0)
+{
+printf("0");
+return (0);
+}
+else
+{
+printf("-")
+return (-1);
+}
+}
