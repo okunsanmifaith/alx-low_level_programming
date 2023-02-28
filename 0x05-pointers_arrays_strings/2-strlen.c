0183c29d8e8018ae1b4ba89979d29247d077b294
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-02-28 13:13:52 -0800

    Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_strlen - Print length of a string
+ *@s : a character
+ *@len: length of string
+ *Rrturn: length of string s
+ **/
+int _strlen(char *s)
+{
+int len;
+len = _strlen(*s);
+
+return (len);
+}
