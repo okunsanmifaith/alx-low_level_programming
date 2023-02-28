3e5b22766302401620bc3dc82fb95c5d4c979ec4
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-02-28 12:35:02 -0800

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - function to take pointer as an int and update value
+ *@n: an integer
+ *Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+int n, *n;
+*n = &n;
+n = 98;
+
+printf("*n = %d", *n);
+return (0);
+}
