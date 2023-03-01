5702ad52d64e3a6a1f35ba4799ff1e6bf61cc937
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-01 11:41:00 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - Display string
+ * @s: String
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int a;
+
+for (a = 0; s[a] != '\0'; a++)
+{
+putchar(str[-a]);
+}
+putchar('\n');
+return;
+}
