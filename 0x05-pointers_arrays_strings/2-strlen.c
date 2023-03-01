11918ba3f6e19d7dab7210269e58b4e8d6102af4
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-02-28 22:16:27 -0800

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,13 +3,15 @@
 /**
  *_strlen - Print length of a string
  *@s : a character
- *@len: length of string
  *Rrturn: length of string s
  **/
 int _strlen(char *s)
 {
-int len;
-len = _strlen(char *s);
-
-return (len);
+int x = 0;
+while (*s != '\0')
+{
+*s++
+x++;
+}
+return (x);
 }
