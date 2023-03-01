9b559ac0afb8831c9bce458770798760b8621a25
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-01 13:09:31 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *puts2 - prints every other character
+ *@str: strings character
+ *
+ *Return: void
+ */
+void puts2(char *str);
+{
+int i;
+char s[i];
+
+for (int i = 0; i < strlen(str); i+=2)
+{
+putchar (str[i]);
+s[i/2] = str[i];
+}
+return 0;
+}
