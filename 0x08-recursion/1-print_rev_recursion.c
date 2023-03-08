e57bb4df54c0be95b7933024d9330b3d3b8fba50
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-08 13:32:47 -0800

    Recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *_print_rev_recursion - print the reverse of a string
+ *@s: string character
+ */
+void _print_rev_recursion(char *s)
+{
+  if (*s != '\0')
+    {
+      _print_rev_recursion(s + 1);
+      printf("%c", s);
+    }
+}
