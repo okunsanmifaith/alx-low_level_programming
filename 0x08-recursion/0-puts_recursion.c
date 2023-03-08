e4f42ded3a8bd4825927fd102bbb1af174b5f2a8
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-08 12:03:10 -0800

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ *_puts_recursion - prints a string
+ *@s: string character
+ */
+void _puts_recursion(char *s)
+{
+printf("%c", s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifmdef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+void _puts_recursion(char *s);
+
+#endif
