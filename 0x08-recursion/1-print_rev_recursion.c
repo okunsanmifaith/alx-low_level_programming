528a89271a76af21fa26aa18902855d2925dfc7a
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-08 13:34:50 -0800

    Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,9 +5,9 @@
  */
 void _print_rev_recursion(char *s)
 {
-  if (*s != '\0')
-    {
-      _print_rev_recursion(s + 1);
-      printf("%c", s);
-    }
+if (*s != '\0')
+{
+_print_rev_recursion(s + 1);
+printf("%c", *s);
+}
 }
