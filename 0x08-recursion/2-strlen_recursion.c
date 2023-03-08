1d308c61624e1c905fd92b04789315848046705e
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-08 13:42:34 -0800

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *_strlen_recursion - prints length of a string
+ *@s: string character
+ *@l: integer
+ *@strlen: length of string
+ */
+int _strlen_recursion(char *s)
+{
+int l;
+
+l = strlen(*s);
+putchar(l);
+}
