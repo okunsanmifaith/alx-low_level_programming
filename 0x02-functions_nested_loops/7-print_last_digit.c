ee9401641993a0b18f357345cfbfd3e8426e722d
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-09 15:48:37 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,13 +7,11 @@
  */
 int print_last_digit(int c)
 {
+int last_digit = c % 10;
 
-	int last_digit = c % 10;
+if (last_digit < 0)
+last_digit *= -1;
+_putchar(last_digit + '0');
 
-	if (last_digit < 0)
-		last_digit *= -1;
-
-	_putchar(last_digit + '0');
-
-	return (last_digit);  
+return (last_digit);  
 }
