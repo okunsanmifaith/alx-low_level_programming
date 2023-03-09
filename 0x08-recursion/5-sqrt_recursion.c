b80530651f283a975649cc8f373fbc07f537d9d5
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-08 19:29:29 -0800

    Recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *_sqrt_recursion - prints the square root of a number
+ *@n: integer
+ *Return: square root or -1 if not square root
+ */
+int _sqrt_recursion(int n)
+{
+return (halp(n, 1));
+}
+/**
+ *halp - function to solve square root
+@c: number to find square root
+*@i: incrementor
+*Return: If n does not have a natural square root, the function should return -1
+*/
+int halp(int c, int i)
+{
+int square;
+
+square = i * i;
+if (square == c)
+return (i);
+else if (square < c)
+return (halp(c, i + 1));
+else
+return (-1);
+}
