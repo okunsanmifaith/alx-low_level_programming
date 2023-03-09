20585084c746555ea2405e2902c6237f62c18546
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-08 18:48:07 -0800

    Resursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *factorial - print factorial of number
+ *@n: integer
+ *Return: factorial
+ */
+int factorial(int n)
+{
+if (n == 0)
+{
+return (1);
+}
+return (n * factorial(n - 1);
+}
