9b55ecb605b9a674ca1c45fba65c2950d1cb518f
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-09 12:07:26 -0800

    Resursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,1 +1,15 @@
-#in
+/**
+ *is_prime_number - prints prime number
+ *@n: an integer
+ *Return: 1 i is prime number, else 0
+ */
+int is_prime_number(int n)
+{
+int i;
+
+for (i = 2; i <= n / 2; i++)
+if (n % i == 0)
+return (1);
+else
+return (0);
+}
