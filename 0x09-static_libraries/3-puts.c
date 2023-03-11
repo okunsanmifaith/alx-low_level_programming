4d3703c8ea8bef767ab08b5166878d5845dffb90
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-11 13:14:02 -0800

    C Statoc Library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,17 @@
 #include "main.h"
+/**
+ *-puts - function name
+ *@str: string character
+ */
 void _puts(char *str)
 #include <stdio.h>
 /**
  * main - Entry point
- *
+ * puts - puts out
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
