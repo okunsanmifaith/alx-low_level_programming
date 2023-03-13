39f1305880aef1c0813d6f907eb5fb47f03aa012
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-13 13:54:28 -0700

    0x0A Argc Argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,12 +6,12 @@
  *@argv: Argument array
  *Return: Always 0
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
 int i = 0;
 while (i <= argc)
 {
-printf("%c", argv[i]);
+printf("%s", argv[i]);
 return (0);
 }
 }
