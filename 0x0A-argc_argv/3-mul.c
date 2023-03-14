93b0a1df1cf64d7296981c6d3cbe7e60f006bf0b
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-14 04:32:11 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main -Entry point
+ *@argc: Argument character
+ *@argv: Argument vector
+ *Return: If the program does not receive two arguments,
+ * your program should print Error, followed by a new line,
+ * and return 1
+ */
+int main(int argc, char *argv[])
+{
+int a, b, c;
+
+if (argc > 1)
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+c = a * b;
+printf("%d", c)
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
