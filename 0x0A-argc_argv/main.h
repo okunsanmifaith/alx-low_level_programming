74f5681ee8cd36e00ee31b670b12d78b0dff7cd0
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-15 14:51:57 -0700

    100-change.c

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -5,5 +5,7 @@
 int main(int argc, char *argv[]);
 int main(int argc, __attribute__((unused)) char *argv[]);
 int main(int argc, char **argv);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
 
 #endif
