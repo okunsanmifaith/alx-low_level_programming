89d9fb9ebfc71687aae2e0a9fe4bfeb2ccff8455
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-22 15:24:37 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  *_strlen - Print length of a string
  *@s : a character
@@ -7,6 +6,11 @@
  */
 int _strlen(char *s)
 {
-printf("%hd", strlen(s));
-return 0;
+int x = 0;
+while (*s != '\0')
+{
+s++;
+x++;
+}
+return (x);
 }
