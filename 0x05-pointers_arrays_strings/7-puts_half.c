acf6d5ef1dee54c40acb6012d5d6ee49d37169ff
Okunsanmi Faith Abike <faitheabinice007@gmail.com>
2023-03-22 16:48:19 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,27 +1,33 @@
 #include "main.h"
+
 /**
- *puts2 - prints every other character
- *@str: pointer character
- *
- *Return: void
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
  */
 void puts_half(char *str)
 {
-int i, c;
+	int len, n, i;
 
-i = 0;
-c = 0;
-while (str[i++])
-{
-c++;
-}
-for (i = 0; i < c; i += 1)
-{
-putchar (*(str + i));
-for (i = 3; i <= 6; i++)
-{
-putchar (*(str + 1));
-}
-putchar ('\n');
-}
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (len % 2)
+	{
+		for (n = (len - 1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
 }
