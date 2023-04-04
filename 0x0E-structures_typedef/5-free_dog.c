a2f9bb8a30a92fe394841acf3c66203c475e25bf
Okunsanmi Faith <faitheabinice007@gmail.com>
2023-04-03 23:53:03 -0700

    Task 4

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *free_dog - function to free dog
+ *@d: structure pointer
+ */
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name)
+free(d->owner);
+free(d);
+}
+}
