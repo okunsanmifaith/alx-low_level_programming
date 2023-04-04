e3b64e7a0d0186fb932fd24d1e80cb52951cd535
Okunsanmi Faith <faitheabinice007@gmail.com>
2023-04-03 23:36:57 -0700

    Task2: dog's love

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "og.h"
 #include <stdlib.h>
+#include "dog,h"
 
 /**
  *print_dog - Function to print structure
@@ -9,13 +9,12 @@
 void print_dog(struct dog *d)
 {
 if (d == NULL)
-return (0);
+return;
 if (d->name == NULL)
 d-> = "(nil)";
-if (d->age == NULL)
-d->age = "(nil)";
 if (d->owner == NULL)
 d->owner = "(nil)";
 }
-printf("Name: %s\n, Age: %f\n, Owner: %s\n", d->name, d->age, d->owner);
+printf("Name: %s\n, Age: %f\n, Owner: %s\n", d->name, d->age,
+d->owner);
 }
