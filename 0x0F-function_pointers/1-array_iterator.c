b1704eb8b70a8b471ffdc2efcb4c87f5709ac85c
Okunsanmi Faith <faitheabinice007@gmail.com>
2023-04-04 13:56:09 -0700

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ *array_iterator - function that executes a function given
+ *as a parameter on each element of an array
+ *@array: pointer to array
+ *@size: size of array
+ *@action: function to pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if (array && size && action)
+{
+for (i = 0; i < size; i++)
+action(*(array + i));
+}
+}
